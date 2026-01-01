ec0534c243c2161df42549a7f975df3d049ec26b
sknoor07 <nooralamshaikh07@gmail.com>
2026-01-01 13:01:41 +0000

    feat: fixed redirecting to dashboard instead of root page while logging in.

--- app/_components/HeroSectionOne.tsx
+++ app/_components/HeroSectionOne.tsx
@@ -125,7 +125,7 @@ const Navbar = () => {
       {!user ? (
         <button
           className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200 cursor-pointer"
-          onClick={() => router.push("/dashboard")}
+          onClick={() => router.push("/sign-in")}
         >
           Login
         </button>
